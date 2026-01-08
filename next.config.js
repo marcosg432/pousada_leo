5dca69aaa89f82bea6c802906ba1f46af401736a
marcosg432 <mg9149303@gmail.com>
2026-01-08 10:40:28 -0400

    Correção: remover images.domains deprecated e adicionar sharp para otimização de imagens

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'images.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
       },
     ],
   },
